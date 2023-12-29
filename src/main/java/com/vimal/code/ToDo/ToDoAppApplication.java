407719f10fe1c94dab6c4972aa84de2c10c5302e
Vimal1464 <vimalpandey1978.78@gmail.com>
2023-12-29 14:04:15 +0530

    Everthing is Working

--- src/main/java/com/vimal/code/ToDo/ToDoAppApplication.java
+++ src/main/java/com/vimal/code/ToDo/ToDoAppApplication.java
@@ -4,9 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class ToDoAppApplication {
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
 	}
